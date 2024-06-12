86ba64477b967f72d0e122adc57af0a79661ab01
Kevin Dwyer <kevin@pheared.net>
2024-06-11 22:35:23 -0400

    100x speed improvement.

--- src/timehash/mod.rs
+++ src/timehash/mod.rs
@@ -1,59 +1,47 @@
-use std::collections::HashMap;
 use std::iter;
 
-static BASE32: &'static str = "01abcdef";
-
-//{'c': 4, 'a': 2, 'e': 6, '0': 0, '1': 1, 'b': 3, 'd': 5, 'f': 7}
-fn get_decode_map() -> HashMap<char, usize> {
-    let mut decode_map: HashMap<char, usize> = HashMap::new();
-    decode_map.insert('0', 0);
-    decode_map.insert('1', 1);
-    decode_map.insert('a', 2);
-    decode_map.insert('b', 3);
-    decode_map.insert('c', 4);
-    decode_map.insert('d', 5);
-    decode_map.insert('e', 6);
-    decode_map.insert('f', 7);
-    return decode_map;
-}
-
-//{'a': ('1', 'b'), 'f': ('e', '0'), 'd': ('c', 'e'), '1': ('0', 'a'), '0': ('f', '1'), 'e': ('d', 'f'), 'c': ('b', 'd'), 'b': ('a', 'c')}
-fn get_neighbor_map() -> HashMap<char, (char, char)> {
-    let mut neighbor_map: HashMap<char, (char, char)> = HashMap::new();
-    neighbor_map.insert('0', ('f', '1'));
-    neighbor_map.insert('1', ('0', 'a'));
-    neighbor_map.insert('a', ('1', 'b'));
-    neighbor_map.insert('b', ('a', 'c'));
-    neighbor_map.insert('c', ('b', 'd'));
-    neighbor_map.insert('d', ('c', 'e'));
-    neighbor_map.insert('e', ('d', 'f'));
-    neighbor_map.insert('f', ('e', '0'));
-    return neighbor_map;
-}
-
-fn create_decode_map() -> HashMap<char, usize> {
-    let before = "f01abcde";
-    let after = "1abcdef0";
-    let mut _decode_map: HashMap<char, usize> = HashMap::new();
-    let mut _neighbor_map = HashMap::new();
-
-    for i in 0..BASE32.len() {
-        let b32_char = BASE32.chars().nth(i).unwrap();
-        println!("{} is {:?}", i, b32_char);
-        _decode_map.insert(b32_char, i);
-        _neighbor_map.insert(b32_char, (before.chars().nth(i).unwrap(), after.chars().nth(i).unwrap()));
+/// Look up the ordinal value for a base32 character
+fn get_ordinal(&c: &char) -> usize {
+    match c {
+        '0' => 0,
+        '1' => 1,
+        'a' => 2,
+        'b' => 3,
+        'c' => 4,
+        'd' => 5,
+        'e' => 6,
+        'f' => 7,
+        _ => panic!("Invalid character")
     }
-    println!("map1: {:?}", _decode_map);
-    println!("map2: {:?}", _neighbor_map);
-    return _decode_map;
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn timehash_decode_map() {
-        use super::*;
-        create_decode_map();
+/// Look up the base32 character corresponding to an ordinal value
+fn get_char(i: usize) -> char {
+    match i {
+        0 => '0',
+        1 => '1',
+        2 => 'a',
+        3 => 'b',
+        4 => 'c',
+        5 => 'd',
+        6 => 'e',
+        7 => 'f',
+        _ => panic!("Invalid index")
+    }
+}
+
+/// Look up the neighboring base32 characters for a given character
+fn get_neighbor_chars(&c: &char) -> (char, char) {
+    match c {
+        '0' => ('f', '1'),
+        '1' => ('0', 'a'),
+        'a' => ('1', 'b'),
+        'b' => ('a', 'c'),
+        'c' => ('b', 'd'),
+        'd' => ('c', 'e'),
+        'e' => ('d', 'f'),
+        'f' => ('e', '0'),
+        _ => panic!("Invalid character")
     }
 }
 
@@ -62,6 +50,14 @@ struct Interval {
     end: f64
 }
 
+/// Encode time in seconds to a timehash string with a given precision.
+/// 
+/// # Examples
+/// 
+/// ```
+/// use timeharsh;
+/// assert_eq!(timeharsh::timehash::encode(1236532473.6328125, 6), "abcdef");
+/// ```
 pub fn encode(timeseconds: f64, precision: usize) -> String {
     let mut timehash: Vec<char> = Vec::new();
 
@@ -83,7 +79,7 @@ pub fn encode(timeseconds: f64, precision: usize) -> String {
         if bit < 2 {
             bit += 1;
         } else {
-            timehash.push(BASE32.chars().nth(ch).unwrap());
+            timehash.push(get_char(ch));
             bit = 0;
             ch = 0;
         }
@@ -92,17 +88,33 @@ pub fn encode(timeseconds: f64, precision: usize) -> String {
     return timehash.into_iter().collect();
 }
 
+/// Decode timehash, returning a single floating point value for epoch seconds.
+/// 
+/// # Examples
+/// 
+/// ```
+/// use timeharsh;
+/// assert_eq!(timeharsh::timehash::decode("abcdef"), 1236532473.6328125);
+/// ```
 pub fn decode(timehash: &str) -> f64 {
     return decode_exactly(timehash).0;
 }
 
+/// Decode timehash, returning a tuple of floating point value for epoch seconds and the plus/minus error for epoch seconds.
+/// Returns a tuple of (epoch_seconds, error_margin).
+/// 
+/// # Examples
+/// 
+/// ```
+/// use timeharsh;
+/// assert_eq!(timeharsh::timehash::decode_exactly("abcdef"), (1236532473.6328125, 7704.4921875));
+/// ```
 pub fn decode_exactly(timehash: &str) -> (f64, f64) {
     let mut interval: Interval = Interval { begin: 0.0, end: 4039372800.0};
     let mut time_error: f64 = (interval.begin + interval.end) / 2.0;
-    let decode_map = get_decode_map();
 
     for c in timehash.chars() {
-        let char_idx = decode_map.get(&c).unwrap();
+        let char_idx = get_ordinal(&c);
         for mask in vec![4, 2, 1] {
             time_error /= 2.0;
 
@@ -117,15 +129,20 @@ pub fn decode_exactly(timehash: &str) -> (f64, f64) {
     return ((interval.begin + interval.end) / 2.0, time_error);
 }
 
-// Return the timehash for the preceding time-window.
+/// Return the timehash for the preceding time-window.
+/// 
+/// # Examples
+/// ```
+/// use timeharsh;
+/// assert_eq!(timeharsh::timehash::before("abcdef"), "abcdee");
+/// ```
 pub fn before(timehash: &str) -> String {
-    let neighbor_map = get_neighbor_map();
     let mut i = 1;
     for c in timehash.chars().rev() {
         let padding: String = iter::repeat("f").take(i - 1).collect();
         let pos = timehash.len() - i;
         if c != '0' {
-            return timehash[0..pos].to_string() + &neighbor_map.get(&c).unwrap().0.to_string() + &padding;
+            return timehash[0..pos].to_string() + &get_neighbor_chars(&c).0.to_string() + &padding;
         } else {
             i += 1;
         }
@@ -133,15 +150,20 @@ pub fn before(timehash: &str) -> String {
     return "".to_string();
 }
 
-// Return the timehash for the succeeding time-window.
+/// Return the timehash for the succeeding time-window.
+/// 
+/// # Examples
+/// ```
+/// use timeharsh;
+/// assert_eq!(timeharsh::timehash::after("abcdef"), "abcdf0");
+/// ```
 pub fn after(timehash: &str) -> String {
-    let neighbor_map = get_neighbor_map();
     let mut i = 1;
     for c in timehash.chars().rev() {
         let padding: String = iter::repeat("0").take(i - 1).collect();
         let pos = timehash.len() - i;
         if c != 'f' {
-            return timehash[0..pos].to_string() + &neighbor_map.get(&c).unwrap().1.to_string() + &padding;
+            return timehash[0..pos].to_string() + &get_neighbor_chars(&c).1.to_string() + &padding;
         } else {
             i += 1;
         }
@@ -149,14 +171,26 @@ pub fn after(timehash: &str) -> String {
     return "".to_string();
 }
 
-// Return the timehashes for the preceding and succeeding time-windows,
-// excluding the timehash for the current time-window.
+/// Return the timehashes for the preceding and succeeding time-windows,
+/// excluding the timehash for the current time-window.
+/// 
+/// # Examples
+/// ```
+/// use timeharsh;
+/// assert_eq!(timeharsh::timehash::neighbors("abcdef"), ("abcdee".to_string(), "abcdf0".to_string()));
+/// ```
 pub fn neighbors(timehash: &str) -> (String, String) {
     return (before(timehash), after(timehash));
 }
 
-// Return the timehashes for the preceding and succeeding time-windows,
-// including the timehash for the current time-window.
+/// Return the timehashes for the preceding and succeeding time-windows,
+/// including the timehash for the current time-window.
+/// 
+/// # Examples
+/// ```
+/// use timeharsh;
+/// assert_eq!(timeharsh::timehash::expand("abcdef"), ("abcdee".to_string(), "abcdef".to_string(), "abcdf0".to_string()));
+/// ```
 pub fn expand(timehash: &str) -> (String, String, String) {
     return (before(timehash), timehash.to_string(), after(timehash));
 }
