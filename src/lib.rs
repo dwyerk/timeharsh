f13fad78c5e073bdd6135446bc5164325a070ec5
Kevin Dwyer <kevin@pheared.net>
2024-06-14 21:52:45 -0400

    Handle errors instead of panicing

--- src/lib.rs
+++ src/lib.rs
@@ -3,49 +3,56 @@ pub mod timehash;
 #[cfg(test)]
 mod tests {
     #[test]
-    fn timehash_encode() {
+    fn timehash_encode() -> Result<(), String> {
         use timehash;
-        assert_eq!(timehash::encode(1487708113.0, 10), "afcccc0e1b");
+        assert_eq!(timehash::encode(1487708113.0, 10).unwrap(), "afcccc0e1b");
+        Ok(())
     }
 
     #[test]
-    fn timehash_encode_precision_length() {
+    fn timehash_encode_precision_length() -> Result<(), String> {
         use timehash;
-        assert_eq!(timehash::encode(1487708113.0, 10).len(), 10);
+        assert_eq!(timehash::encode(1487708113.0, 10)?.len(), 10);
+        Ok(())
     }
 
     #[test]
-    fn timehash_decode() {
+    fn timehash_decode() -> Result<(), String> {
         use timehash;
-        assert_eq!(timehash::decode("afcccc0e1b"), 1487708111.2923145);
+        assert_eq!(timehash::decode("afcccc0e1b")?, 1487708111.2923145);
+        Ok(())
     }
 
     #[test]
-    fn timehash_before() {
+    fn timehash_before() -> Result<(), String> {
         use timehash;
-        assert_eq!(timehash::before("afcccc0e1b"), "afcccc0e1a");
-        assert_eq!(timehash::before("afcccc0e1a"), "afcccc0e11");
-        assert_eq!(timehash::before("afcccc0e10"), "afcccc0e0f");
+        assert_eq!(timehash::before("afcccc0e1b")?, "afcccc0e1a");
+        assert_eq!(timehash::before("afcccc0e1a")?, "afcccc0e11");
+        assert_eq!(timehash::before("afcccc0e10")?, "afcccc0e0f");
+        Ok(())
     }
 
     #[test]
-    fn timehash_after() {
+    fn timehash_after() -> Result<(), String> {
         use timehash;
-        assert_eq!(timehash::after("afcccc0e0f"), "afcccc0e10");
-        assert_eq!(timehash::after("afcccc0e1a"), "afcccc0e1b");
-        assert_eq!(timehash::after("afcccc0e1b"), "afcccc0e1c");
+        assert_eq!(timehash::after("afcccc0e0f")?, "afcccc0e10");
+        assert_eq!(timehash::after("afcccc0e1a")?, "afcccc0e1b");
+        assert_eq!(timehash::after("afcccc0e1b")?, "afcccc0e1c");
+        Ok(())
     }
 
     #[test]
-    fn timehash_neighbors() {
+    fn timehash_neighbors() -> Result<(), String> {
         use timehash;
-        assert_eq!(timehash::neighbors("afcccc0e0f"), ("afcccc0e0e".to_string(), "afcccc0e10".to_string()));
+        assert_eq!(timehash::neighbors("afcccc0e0f")?, ("afcccc0e0e".to_string(), "afcccc0e10".to_string()));
+        Ok(())
     }
 
     #[test]
-    fn timehash_expand() {
+    fn timehash_expand() -> Result<(), String> {
         use timehash;
-        assert_eq!(timehash::expand("afcccc0e0f"), ("afcccc0e0e".to_string(), "afcccc0e0f".to_string(), "afcccc0e10".to_string()));
+        assert_eq!(timehash::expand("afcccc0e0f")?, ("afcccc0e0e".to_string(), "afcccc0e0f".to_string(), "afcccc0e10".to_string()));
+        Ok(())
     }
 }
 
--- src/timehash/mod.rs
+++ src/timehash/mod.rs
@@ -1,47 +1,47 @@
 use std::iter;
 
 /// Look up the ordinal value for a base32 character
-fn get_ordinal(&c: &char) -> usize {
+fn get_ordinal(&c: &char) -> Result<usize, String> {
     match c {
-        '0' => 0,
-        '1' => 1,
-        'a' => 2,
-        'b' => 3,
-        'c' => 4,
-        'd' => 5,
-        'e' => 6,
-        'f' => 7,
-        _ => panic!("Invalid character")
+        '0' => Ok(0),
+        '1' => Ok(1),
+        'a' => Ok(2),
+        'b' => Ok(3),
+        'c' => Ok(4),
+        'd' => Ok(5),
+        'e' => Ok(6),
+        'f' => Ok(7),
+        _ => Err(format!("Invalid character: {c}"))
     }
 }
 
 /// Look up the base32 character corresponding to an ordinal value
-fn get_char(i: usize) -> char {
+fn get_char(i: usize) -> Result<char, String> {
     match i {
-        0 => '0',
-        1 => '1',
-        2 => 'a',
-        3 => 'b',
-        4 => 'c',
-        5 => 'd',
-        6 => 'e',
-        7 => 'f',
-        _ => panic!("Invalid index")
+        0 => Ok('0'),
+        1 => Ok('1'),
+        2 => Ok('a'),
+        3 => Ok('b'),
+        4 => Ok('c'),
+        5 => Ok('d'),
+        6 => Ok('e'),
+        7 => Ok('f'),
+        _ => Err(format!("Invalid ordinal value: {i}"))
     }
 }
 
 /// Look up the neighboring base32 characters for a given character
-fn get_neighbor_chars(&c: &char) -> (char, char) {
+fn get_neighbor_chars(&c: &char) -> Result<(char, char), String> {
     match c {
-        '0' => ('f', '1'),
-        '1' => ('0', 'a'),
-        'a' => ('1', 'b'),
-        'b' => ('a', 'c'),
-        'c' => ('b', 'd'),
-        'd' => ('c', 'e'),
-        'e' => ('d', 'f'),
-        'f' => ('e', '0'),
-        _ => panic!("Invalid character")
+        '0' => Ok(('f', '1')),
+        '1' => Ok(('0', 'a')),
+        'a' => Ok(('1', 'b')),
+        'b' => Ok(('a', 'c')),
+        'c' => Ok(('b', 'd')),
+        'd' => Ok(('c', 'e')),
+        'e' => Ok(('d', 'f')),
+        'f' => Ok(('e', '0')),
+        _ => Err(format!("Invalid character: {c}"))
     }
 }
 
@@ -55,14 +55,14 @@ struct Interval {
 /// # Examples
 /// 
 /// ```
-/// use timeharsh;
-/// assert_eq!(timeharsh::timehash::encode(1236532473.6328125, 6), "abcdef");
+/// use timeharsh::timehash;
+/// assert_eq!(timehash::encode(1236532473.6328125, 6).unwrap(), "abcdef");
 /// ```
-pub fn encode(timeseconds: f64, precision: usize) -> String {
+pub fn encode(timeseconds: f64, precision: usize) -> Result<String, String> {
     let mut timehash: Vec<char> = Vec::new();
 
     let mut interval: Interval = Interval { begin: 0.0, end: 4039372800.0};
-    let bits = vec![4, 2, 1];
+    let bits: [usize; 3] = [4, 2, 1];
     let mut bit = 0;
     let mut ch = 0;
 
@@ -79,13 +79,13 @@ pub fn encode(timeseconds: f64, precision: usize) -> String {
         if bit < 2 {
             bit += 1;
         } else {
-            timehash.push(get_char(ch));
+            timehash.push(get_char(ch)?);
             bit = 0;
             ch = 0;
         }
     }
 
-    return timehash.into_iter().collect();
+    return Ok(timehash.into_iter().collect());
 }
 
 /// Decode timehash, returning a single floating point value for epoch seconds.
@@ -94,10 +94,10 @@ pub fn encode(timeseconds: f64, precision: usize) -> String {
 /// 
 /// ```
 /// use timeharsh;
-/// assert_eq!(timeharsh::timehash::decode("abcdef"), 1236532473.6328125);
+/// assert_eq!(timeharsh::timehash::decode("abcdef").unwrap(), 1236532473.6328125);
 /// ```
-pub fn decode(timehash: &str) -> f64 {
-    return decode_exactly(timehash).0;
+pub fn decode(timehash: &str) -> Result<f64, String> {
+    return Ok(decode_exactly(timehash)?.0);
 }
 
 /// Decode timehash, returning a tuple of floating point value for epoch seconds and the plus/minus error for epoch seconds.
@@ -106,16 +106,17 @@ pub fn decode(timehash: &str) -> f64 {
 /// # Examples
 /// 
 /// ```
-/// use timeharsh;
-/// assert_eq!(timeharsh::timehash::decode_exactly("abcdef"), (1236532473.6328125, 7704.4921875));
+/// use timeharsh::timehash;
+/// assert_eq!(timehash::decode_exactly("abcdef").unwrap(), (1236532473.6328125, 7704.4921875));
 /// ```
-pub fn decode_exactly(timehash: &str) -> (f64, f64) {
+pub fn decode_exactly(timehash: &str) -> Result<(f64, f64), String> {
     let mut interval: Interval = Interval { begin: 0.0, end: 4039372800.0};
     let mut time_error: f64 = (interval.begin + interval.end) / 2.0;
-
+    let masks: [usize; 3] = [4, 2, 1];
+ 
     for c in timehash.chars() {
-        let char_idx = get_ordinal(&c);
-        for mask in vec![4, 2, 1] {
+        let char_idx = get_ordinal(&c)?;
+        for mask in masks {
             time_error /= 2.0;
 
             let mid = (interval.begin + interval.end) / 2.0;
@@ -126,49 +127,49 @@ pub fn decode_exactly(timehash: &str) -> (f64, f64) {
             }
         }
     }
-    return ((interval.begin + interval.end) / 2.0, time_error);
+    return Ok(((interval.begin + interval.end) / 2.0, time_error));
 }
 
 /// Return the timehash for the preceding time-window.
 /// 
 /// # Examples
 /// ```
-/// use timeharsh;
-/// assert_eq!(timeharsh::timehash::before("abcdef"), "abcdee");
+/// use timeharsh::timehash;
+/// assert_eq!(timehash::before("abcdef").unwrap(), "abcdee");
 /// ```
-pub fn before(timehash: &str) -> String {
+pub fn before(timehash: &str) -> Result<String, String> {
     let mut i = 1;
     for c in timehash.chars().rev() {
         let padding: String = iter::repeat("f").take(i - 1).collect();
         let pos = timehash.len() - i;
         if c != '0' {
-            return timehash[0..pos].to_string() + &get_neighbor_chars(&c).0.to_string() + &padding;
+            return Ok(timehash[0..pos].to_string() + &get_neighbor_chars(&c)?.0.to_string() + &padding);
         } else {
             i += 1;
         }
     }
-    return "".to_string();
+    return Ok("".to_string());
 }
 
 /// Return the timehash for the succeeding time-window.
 /// 
 /// # Examples
 /// ```
-/// use timeharsh;
-/// assert_eq!(timeharsh::timehash::after("abcdef"), "abcdf0");
+/// use timeharsh::timehash;
+/// assert_eq!(timehash::after("abcdef").unwrap(), "abcdf0");
 /// ```
-pub fn after(timehash: &str) -> String {
+pub fn after(timehash: &str) -> Result<String, String> {
     let mut i = 1;
     for c in timehash.chars().rev() {
         let padding: String = iter::repeat("0").take(i - 1).collect();
         let pos = timehash.len() - i;
         if c != 'f' {
-            return timehash[0..pos].to_string() + &get_neighbor_chars(&c).1.to_string() + &padding;
+            return Ok(timehash[0..pos].to_string() + &get_neighbor_chars(&c)?.1.to_string() + &padding);
         } else {
             i += 1;
         }
     }
-    return "".to_string();
+    return Ok("".to_string());
 }
 
 /// Return the timehashes for the preceding and succeeding time-windows,
@@ -176,11 +177,11 @@ pub fn after(timehash: &str) -> String {
 /// 
 /// # Examples
 /// ```
-/// use timeharsh;
-/// assert_eq!(timeharsh::timehash::neighbors("abcdef"), ("abcdee".to_string(), "abcdf0".to_string()));
+/// use timeharsh::timehash;
+/// assert_eq!(timehash::neighbors("abcdef").unwrap(), ("abcdee".to_string(), "abcdf0".to_string()));
 /// ```
-pub fn neighbors(timehash: &str) -> (String, String) {
-    return (before(timehash), after(timehash));
+pub fn neighbors(timehash: &str) -> Result<(String, String), String> {
+    return Ok((before(timehash)?, after(timehash)?));
 }
 
 /// Return the timehashes for the preceding and succeeding time-windows,
@@ -188,9 +189,9 @@ pub fn neighbors(timehash: &str) -> (String, String) {
 /// 
 /// # Examples
 /// ```
-/// use timeharsh;
-/// assert_eq!(timeharsh::timehash::expand("abcdef"), ("abcdee".to_string(), "abcdef".to_string(), "abcdf0".to_string()));
+/// use timeharsh::timehash;
+/// assert_eq!(timehash::expand("abcdef").unwrap(), ("abcdee".to_string(), "abcdef".to_string(), "abcdf0".to_string()));
 /// ```
-pub fn expand(timehash: &str) -> (String, String, String) {
-    return (before(timehash), timehash.to_string(), after(timehash));
+pub fn expand(timehash: &str) -> Result<(String, String, String), String> {
+    return Ok((before(timehash)?, timehash.to_string(), after(timehash)?));
 }
